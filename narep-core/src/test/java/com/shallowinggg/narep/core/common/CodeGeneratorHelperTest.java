e118f3fc29d4fdc97fe903fe2edb882365b95990
shallowinggg <shallowinggg@icloud.com>
2020-01-12 22:28:31 +0800

    Initial Commit

---
+++ narep-core/src/test/java/com/shallowinggg/narep/core/common/CodeGeneratorHelperTest.java
@@ -0,0 +1,61 @@
+package com.shallowinggg.narep.core.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static com.shallowinggg.narep.core.common.GeneratorConfig.DOUBLE_LINE_SEPARATOR;
+import static com.shallowinggg.narep.core.common.GeneratorConfig.LINE_SEPARATOR;
+
+public class CodeGeneratorHelperTest {
+
+    @Test
+    public void testBuildFullQualifiedName() {
+        String val = CodeGeneratorHelper.buildFullQualifiedName("com.example", "App");
+        Assert.assertEquals(val, "com.example.App");
+
+        val = CodeGeneratorHelper.buildFullQualifiedName("com.example",
+                "common", "App");
+        Assert.assertEquals(val, "com.example.common.App");
+    }
+
+    @Test
+    public void testBuildPackage() {
+        String val = CodeGeneratorHelper.buildDefaultPackage("com.example");
+        Assert.assertEquals(val, "package com.example;" + DOUBLE_LINE_SEPARATOR);
+
+        val = CodeGeneratorHelper.buildSubPackage("com.example", "common");
+        Assert.assertEquals(val, "package com.example.common;"  + DOUBLE_LINE_SEPARATOR);
+    }
+
+    @Test
+    public void testBuildInterfaceDeclaration() {
+        String val = CodeGeneratorHelper.buildInterfaceDeclaration("CodeGenerator");
+        Assert.assertEquals(val, "public interface CodeGenerator {" + LINE_SEPARATOR);
+
+        val = CodeGeneratorHelper.buildInterfaceDeclaration("JavaCodeGenerator", "CodeGenerator");
+        Assert.assertEquals(val, "public interface JavaCodeGenerator extend CodeGenerator {" + LINE_SEPARATOR);
+    }
+
+    @Test
+    public void testBuildClassDeclaration() {
+        String val = CodeGeneratorHelper.buildClassDeclaration("ClassCodeGenerator");
+        Assert.assertEquals(val, "public class ClassCodeGenerator {" + LINE_SEPARATOR);
+
+        val = CodeGeneratorHelper.buildClassDeclaration("PairCodeGenerator", "ClassCodeGenerator");
+        Assert.assertEquals(val, "public class PairCodeGenerator extend ClassCodeGenerator {" + LINE_SEPARATOR);
+    }
+
+    @Test
+    public void testBuildEnumDeclaration() {
+        String val = CodeGeneratorHelper.buildEnumDeclaration("TlsMode");
+        Assert.assertEquals(val, "public enum TlsMode {" + LINE_SEPARATOR);
+    }
+
+    @Test
+    public void testBuildGenericClassDeclaration() {
+        String val = CodeGeneratorHelper.buildGenericClassDeclaration("Pair", Arrays.asList("T1", "T2"));
+        Assert.assertEquals(val, "public class Pair<T1, T2> {" + LINE_SEPARATOR);
+    }
+}
